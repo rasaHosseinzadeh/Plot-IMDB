e9a38a23e75883abe5d0bf821ae13593640c2e5e
Rasa Hosseinzdaeh <rasa.hosseinzadeh@gmail.com>
2023-11-19 02:26:47 -0500

    Make compatible with new imdb look.

--- imdb-chart.py
+++ imdb-chart.py
@@ -7,45 +7,37 @@ from urllib import request
 import plotly.express as px
 import sys
 
+headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537'}
 
 def crawle_imdb_season(base_url, season) -> list:
     url = base_url+season
     sys.stderr.write(f"Crawling Season {season}.\n")
     Episode = namedtuple('Episode', ['season', 'num', 'name', 'vote', 'rate'])
-    with request.urlopen(url) as response:
+
+    req = request.Request(url=url, headers=headers)
+    with request.urlopen(req) as response:
         page = response.read()
-        tree = html.fromstring(page)
-        s_ep = (
-            (int(x.text.split(",")[0][1:]),
-             int(x.text.split(",")[1][3:])) for x in
-            tree.xpath('//*[contains(@class,"zero-z-index")]//div'))
-        name = (x.text for x in
-                tree.xpath("//div[contains(@class,'eplist')]//strong/a"))
-        vote = (int(x.text.translate({ord(c): None for c in "(),"})) for x in
-                tree.xpath("//span[@class='ipl-rating-star__total-votes']"))
-        rating = (float(x.text) for x in
-                  tree.xpath("//div/div[1]/span[@class='ipl-rating-star__rating']"))
-    return (Episode(s, e, n, v, r) for ((s, e), n, v, r) in
-            zip(s_ep, name, vote, rating))
+    tree = html.fromstring(page)
+    s_e_n = tree.xpath("//div[@class='ipc-title__text']")
+    season = [int(x.text_content().split('.')[0][1:]) for x in s_e_n]
+    episode = [int(x.text_content().split('.')[1].split('∙')[0].strip()[1:]) for x in s_e_n]
+    name = [x.text_content().split('.')[1].split('∙')[1].strip() for x in s_e_n]
+    vote_count_translate = lambda x: int( float(x[:-1])*1000) if 'K' in x else int(x)
+    vote = [vote_count_translate(v.text_content()[2:-1]) for v in tree.xpath("//span[@class='ipc-rating-star--voteCount']")]
+    rate = [float(x.text_content().split('/')[0]) for x in tree.xpath("//div[@data-testid='ratingGroup--container']")]
+    return [Episode(s, e, n, v, r) for (s, e, n, v, r) in
+            zip(season, episode, name, vote, rate)]
 
 
 def crawl_imdb_series(base_url: str, season_count: int) -> list:
-    return (eps for i in range(1, season_count + 1) for eps in
-            crawle_imdb_season(base_url, str(i)))
-
-
-def get_season_count(base_url: str) -> int:
-    season_count = 0
-    with request.urlopen(base_url+"0") as response:
-        page = response.read()
-        tree = html.fromstring(page)
-        options = tree.xpath("//select[@id='bySeason']//option")
+    res = []
+    for s in range(1, season_count + 1):
         try:
-            season_count = int(options[-1].text)
+            new_season = crawle_imdb_season(base_url, str(s))
+            res += new_season
         except:
-            print("Can't determine season count please enter manually.\n")
-            season_count = int(input())
-    return season_count
+            pass
+    return res
 
 
 def plot(series, name) -> None:
@@ -92,11 +84,25 @@ def plot(series, name) -> None:
     fig.write_html(f"./{name}.html")
 
 
+def get_season_count(base_url: str) -> int:
+    season_count = 0
+    req = request.Request(url=base_url, headers=headers)
+    with request.urlopen(req) as response:
+        page = response.read()
+        tree = html.fromstring(page)
+        options = tree.xpath("//li[@data-testid='tab-season-entry']")
+        try:
+            season_count = int(options[-1].text)
+        except:
+            print("Can't determine season count please enter manually.\n")
+            season_count = int(input())
+    return season_count
+
+
 if __name__ == "__main__":
     series_id = sys.argv[1]
     name = sys.argv[2]
-    base_url = "https://www.imdb.com/title/{}/episodes?season=".format(
-        series_id)
+    base_url = f"https://www.imdb.com/title/{series_id}/episodes/?season="
     sys.stderr.write("Get Season count.\n")
     season_count = get_season_count(base_url)
     series = DataFrame(data=crawl_imdb_series(base_url, season_count))
